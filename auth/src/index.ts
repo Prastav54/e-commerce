7f2381adcd346f4f7b290a71cfe77f5c811b2858
Prastav <pokharelprastav@gmail.com>
2022-12-23 11:33:47 +0545

    Mongoose deployment creater and skaffold config file written

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors';
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
 import { signupRouter } from './routes/signup';
@@ -19,6 +20,17 @@ app.all('*', () => {
 })
 app.use(errorHHandler);
 
-app.listen(3000, () => {
-    console.log('Listening to port 3000!')
-})
+const start = async () => {
+    try {  
+        await mongoose.connect('mongodb://auth-mongo-srv:27017/auth')
+        console.log('Connected to mongo')
+    } catch (error) {
+        console.log(error);
+    }
+
+    app.listen(3000, () => {
+        console.log('Listening to port 3000!')
+    })
+}
+
+start();
